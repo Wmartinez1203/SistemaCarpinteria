c967f5f07a4b8ee0421cad92aefbb6084b1e9db1
Fernando Martinez <Wmartinez1203@users.noreply.github.com>
2026-03-26 14:21:27 -0500

    build(config): 🛠️ configurar entornos de desarrollo y variables globales de Angular

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://127.0.0.1:5000',
+  googleClientId: '286803224541-tuns40h7tvh47cumqqa0r4fjfi0ddgnl.apps.googleusercontent.com'
+};
